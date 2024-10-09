072c2bc381caaed0d4cee9a51f9000338d903b8b
Commings-Jie <2167316266@qq.com>
2024-10-09 18:10:24 +0800

    Site updated: 2024-10-09 18:10:24

--- js/custom.js
+++
@@ -1,3 +0,0 @@
-//cdn.jsdelivr.net/gh/EmoryHuang/BlogBeautify@1.1/DynamicRibbon.min.js
-//cdn.jsdelivr.net/gh/EmoryHuang/BlogBeautify@1.1/snowflake.min.js
-//cdn.jsdelivr.net/gh/EmoryHuang/BlogBeautify@1.1/star.min.js 
